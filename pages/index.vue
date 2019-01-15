a1bf66b9d2fb95feae076b8187f0279fc519ae18
CarloGammarota <carlo.gammarota@gmail.com>
2019-01-15 04:18:49 -0300

    startProject

---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <!--<section class="container">-->
+      <!--<div>-->
+        <!--<logo/>-->
+        <!--<h1 class="title">-->
+          <!--buscoDepto-->
+        <!--</h1>-->
+        <!--<h2 class="subtitle">-->
+          <!--deptos dueño directo-->
+        <!--</h2>-->
+        <!--<div class="links">-->
+          <!--<a-->
+            <!--href="https://nuxtjs.org/"-->
+            <!--target="_blank"-->
+            <!--class="button&#45;&#45;green">Documentation</a>-->
+          <!--<a-->
+            <!--href="https://github.com/nuxt/nuxt.js"-->
+            <!--target="_blank"-->
+            <!--class="button&#45;&#45;grey">GitHub</a>-->
+        <!--</div>-->
+      <!--</div>-->
+    <!--</section>-->
+
+    <Product></Product>
+  </div>
+</template>
+
+<script>
+
+  //Components
+  import Logo from '~/components/Logo.vue'
+
+  import Product from "./product";
+
+
+export default {
+  components: {
+    Product,
+    Logo
+  }
+}
+</script>
+
+<style>
+
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ pages/product.vue
@@ -0,0 +1,98 @@
+<template>
+
+        <div class="columns is-full">
+          <!--<div class="column is-4">-->
+            <!--First column-->
+          <!--</div>-->
+
+          <div class="column is-4" v-for="(item, index) in products">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img :src="item.img" alt="Placeholder image">
+                </figure>
+              </div>
+              <div class="card-content">
+                <!--<div class="media">-->
+                  <!--<div class="media-left">-->
+                    <!--<figure class="image is-48x48">-->
+                      <!--<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">-->
+                    <!--</figure>-->
+                  <!--</div>-->
+                  <!--<div class="media-content">-->
+                    <!--<p class="title is-4">John Smith</p>-->
+                    <!--<p class="subtitle is-6">@johnsmith</p>-->
+                  <!--</div>-->
+                <!--</div>-->
+
+                <h3 class="price">${{item.price}} /Per Month</h3>
+
+                <div class="content">
+                  <p class="des">{{item.des}}</p>
+                  <p class="address"><fa :icon="['fas', 'dollar-sign']" /> {{item.address}}</p>
+
+
+                  <br>
+                  <div class="columns info">
+                    <div class="column is-3">
+                      <fa :icon="['fas', 'dollar-sign']" /> Beds: {{item.Beds}}
+
+                    </div>
+                    <div class="column is-3">
+                      <fa :icon="['fas', 'dollar-sign']" /> Baths: {{item.Baths}}
+                    </div>
+                    <div class="column">
+                      <fa :icon="['fas', 'dollar-sign']" /> Area: {{item.Area}}
+                    </div>
+                  </div>
+                  <a>@{{item.author}}</a>
+                  <a href="#">#casa</a> <a href="#">#pileta</a>
+                  <time datetime="2017-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!--<div class="column is-4 is-0color">-->
+            <!--Third column-->
+            <!--<fa :icon="['fas', 'dollar-sign']" />-->
+          <!--</div>-->
+        </div>
+
+</template>
+
+<script>
+    export default {
+        name: "product",
+        data(){
+          return {
+            products: [
+              {author: "Carlo Gammarota", img:"http://www.nordestenoticias.com.ar/wp-content/uploads/2018/08/01-4.jpg", price: 10000, des:"House in Kent Street", address:"127 Kent Street, Sydny, NEW 2000", Beds:3, Baths:2, Area:"587 sq ft"},
+              {author: "Carlo Gammarota", img:"http://www.nordestenoticias.com.ar/wp-content/uploads/2018/08/01-4.jpg", price: 10000, des:"House in Kent Street", address:"127 Kent Street, Sydny, NEW 2000", Beds:3, Baths:2, Area:"587 sq ft"},
+              {author: "Carlo Gammarota", img:"http://www.nordestenoticias.com.ar/wp-content/uploads/2018/08/01-4.jpg", price: 10000, des:"House in Kent Street", address:"127 Kent Street, Sydny, NEW 2000", Beds:3, Baths:2, Area:"587 sq ft"},
+              {author: "Carlo Gammarota", img:"http://www.nordestenoticias.com.ar/wp-content/uploads/2018/08/01-4.jpg", price: 10000, des:"House in Kent Street", address:"127 Kent Street, Sydny, NEW 2000", Beds:3, Baths:2, Area:"587 sq ft"},
+              {author: "Carlo Gammarota", img:"http://www.nordestenoticias.com.ar/wp-content/uploads/2018/08/01-4.jpg", price: 10000, des:"House in Kent Street", address:"127 Kent Street, Sydny, NEW 2000", Beds:3, Baths:2, Area:"587 sq ft"},
+              {author: "Carlo Gammarota", img:"http://www.nordestenoticias.com.ar/wp-content/uploads/2018/08/01-4.jpg", price: 10000, des:"House in Kent Street", address:"127 Kent Street, Sydny, NEW 2000", Beds:3, Baths:2, Area:"587 sq ft"},
+            ]
+            }
+          }
+    }
+</script>
+
+<style scoped>
+  .info{
+    font-size: 15px;
+  }
+  .price{
+    color: dodgerblue;
+    font-size: 25px;
+  }
+  .des{
+    font-size: 15px;
+
+  }
+  .address{
+    font-size: 13px;
+    color: #7f828b;
+
+  }
+</style>
